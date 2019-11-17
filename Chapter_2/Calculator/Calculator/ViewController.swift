a3b543d8568a15184b03c5e465aeff6685f4f1de
kortasn <kortasn@github.com>
2019-11-17 12:50:47 +0100

    Calculator exercise - Auto layout

---
+++ Chapter_2/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nesrine Kortas on 12.11.19.
+//  Copyright © 2019 Nesrine Kortas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
